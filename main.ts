f7dc1677f61a9c0d9164f0c148a133679b395bcd
Sam-MARTis <samanthmartis@gmail.com>
2025-02-12 20:09:43 +0530

    testing

--- main.ts
+++ main.ts
@@ -3,3 +3,6 @@ const canvas = document.getElementById("projectCanvas") as HTMLCanvasElement;
 canvas.width = window.innerWidth*devicePixelRatio;
 canvas.height = window.innerHeight*devicePixelRatio;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
+
+const arr = new Float32Array(100*100)
+
