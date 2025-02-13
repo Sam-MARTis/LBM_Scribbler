d9bbd45f168f9ac87490b425313d6abd1f7da98e
Dawar13 <dawar.iitb@gmail.com>
2025-02-13 23:15:42 +0530

    add the new array types

--- main.ts
+++ main.ts
@@ -6,3 +6,28 @@ const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
 
 const arr = new Float32Array(100*100)
 
+//microscopic variables used here for dircetional distribution of velocity
+const height = 100;
+const width = 100;  // i dont know the scale of these particles probability so this is an example value 
+
+const size = height * width;
+
+const n0  = new Float32Array(size).fill(0); // Naught
+const nN  = new Float32Array(size).fill(0); // North
+const nS  = new Float32Array(size).fill(0); // South
+const nE  = new Float32Array(size).fill(0); // East
+const nW  = new Float32Array(size).fill(0); // West
+const nNW = new Float32Array(size).fill(0); // Northwest
+const nNE = new Float32Array(size).fill(0); // Northeast
+const nSE = new Float32Array(size).fill(0); // Southeast
+const nSW = new Float32Array(size).fill(0); // Southwest
+
+// Barriers
+const barrier = new Int8Array(size).fill(0); // Barriers
+
+// Macroscopic density and velocity
+const rho    = new Float32Array(size).fill(0); // Cell density
+const ux     = new Float32Array(size).fill(0); // Cell x-velocity
+const uy     = new Float32Array(size).fill(0); // Cell y-velocity
+const speed2 = new Float32Array(size).fill(0); // Cell squared velocity
+
