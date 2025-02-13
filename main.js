62980e55f35b1bcd0ee66c52a3a39e5f8f2a0378
onemaster001 <onemaster787@gmail.com>
2025-02-14 00:10:05 +0530

    Fun as coordinate converter

--- main.js
+++ main.js
@@ -5,25 +5,36 @@ canvas.height = window.innerHeight * devicePixelRatio;
 const ctx = canvas.getContext("2d");
 const WIDTH = 256;
 const HEIGHT = 64;
-const array1 = new Float32Array(WIDTH * HEIGHT);
+const array1 = new Float32Array(WIDTH * HEIGHT).fill(0);
 // const n0 =new Float32Array(WIDTH*HEIGHT)
 const viscosity = 0.02;
 const omega = 1. / (3 * viscosity + 0.5);
 const u0 = 0.1;
 // Directions
-const n0 = new Float32Array(WIDTH * HEIGHT);
-const nN = new Float32Array(WIDTH * HEIGHT);
-const nS = new Float32Array(WIDTH * HEIGHT);
-const nE = new Float32Array(WIDTH * HEIGHT);
-const nW = new Float32Array(WIDTH * HEIGHT);
-const nNE = new Float32Array(WIDTH * HEIGHT);
-const nNW = new Float32Array(WIDTH * HEIGHT);
-const nSE = new Float32Array(WIDTH * HEIGHT);
-const nSW = new Float32Array(WIDTH * HEIGHT);
+const n0 = new Float32Array(WIDTH * HEIGHT).fill(0);
+const nN = new Float32Array(WIDTH * HEIGHT).fill(0);
+const nS = new Float32Array(WIDTH * HEIGHT).fill(0);
+const nE = new Float32Array(WIDTH * HEIGHT).fill(0);
+const nW = new Float32Array(WIDTH * HEIGHT).fill(0);
+const nNE = new Float32Array(WIDTH * HEIGHT).fill(0);
+const nNW = new Float32Array(WIDTH * HEIGHT).fill(0);
+const nSE = new Float32Array(WIDTH * HEIGHT).fill(0);
+const nSW = new Float32Array(WIDTH * HEIGHT).fill(0);
 // Barriers
-const bar = new Float32Array(WIDTH * HEIGHT);
+const bar = new Float32Array(WIDTH * HEIGHT).fill(0);
 // Macro
-const rho = new Float32Array(WIDTH * HEIGHT);
-const ux = new Float32Array(WIDTH * HEIGHT);
-const uy = new Float32Array(WIDTH * HEIGHT);
-const speed2 = new Float32Array(WIDTH * HEIGHT);
+const rho = new Float32Array(WIDTH * HEIGHT).fill(0);
+const ux = new Float32Array(WIDTH * HEIGHT).fill(0);
+const uy = new Float32Array(WIDTH * HEIGHT).fill(0);
+const speed2 = new Float32Array(WIDTH * HEIGHT).fill(0);
+function converter(x, y) {
+    let n = (y * WIDTH) + x;
+    console.log(n);
+}
+converter(2, 5);
+function converter2(m) {
+    let y = (m / WIDTH);
+    let x = m % WIDTH;
+    console.log(Math.floor(y), x);
+}
+converter2(1282);
